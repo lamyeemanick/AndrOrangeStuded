d2a4aa0aec8d5c0954d9b29336e8360131863b76
lamyeemanick <nick.lam-yee-man@epitech.eu>
2020-08-19 16:25:53 +0900

    comit initial

---
+++ AndrOrangeStuded/src/main/java/com/orange/androrangestuded/Test.java
@@ -0,0 +1,12 @@
+package com.orange.androrangestuded;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Test {
+    public static void s(Context c, String message) {
+
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+
+    }
+}
